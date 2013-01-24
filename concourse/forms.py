0ba430f13454a73607571267f49dab9c87127b68
Jonathon W. Marshall <jonathon.marshall@gmail.com>
2013-01-25 03:11:59 +0400

    extended UserCreationForm

--- concourse/forms.py
+++ concourse/forms.py
@@ -1,9 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    tos = forms.BooleanField(
+        label = _("Terms of Service"),
+        required = True
+    )
+    privacy = forms.BooleanField(
+        label = _("Privacy Policy"),
+        required = True
+    )
+    username = forms.RegexField(
+        label = _("Username"),
+        max_length = 30,
+        regex = r'^[\w_]+$',
+        help_text = _("30 character limit, alphanumeric only."),
+        error_messages = {
+            'invalid': _("Invalid username. Alphanumeric characters only.")
+        }
+    )
+    password2 = forms.CharField(
+        label = _("Password confirmation"),
+        widget = forms.PasswordInput,
+        help_text=_("Enter the same password as above.")
+    )
 
     class Meta:
         model = User
